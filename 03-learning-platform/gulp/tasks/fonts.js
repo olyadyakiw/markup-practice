dab55e176dec38cafcc31ef78044b51dc59d8b35
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-07-29 13:41:47 +0200

    add 03-learning-platform (setup project)

---
+++ 03-learning-platform/gulp/tasks/fonts.js
@@ -0,0 +1,33 @@
+import fonter from "gulp-fonter-fix";
+import ttf2woff2 from "gulp-ttf2woff2";
+
+export const otfToTtf = () => {
+    return app.gulp.src(`${app.path.srcFolder}/fonts/*.otf`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: 'FONTS',
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
+}
+
+export const ttfToWoff2 = () => {
+    return app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: 'FONTS',
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(fonter({
+            formats: ['woff']
+        }))
+        .pipe(app.gulp.dest(`${app.path.build.fonts}`))
+        .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
+        .pipe(ttf2woff2())
+        .pipe(app.gulp.dest(`${app.path.build.fonts}`));
+}
