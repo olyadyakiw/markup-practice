6842c7f4350e75866edb8df6184898476dc2dcc5
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-10-29 16:06:31 +0100

    06-pod-of-cast: add header

--- 06-pod-of-cast/src/js/app.js
+++ 06-pod-of-cast/src/js/app.js
@@ -1,32 +1,33 @@
 document.addEventListener('load', () => {
-    isWebp();
+    isWebp()
     // isElementExist("class", function);
 })
 
-function isElementExist(_el, _cb) {
-	var elem = document.querySelector(_el);
+initMenu()
 
-	if (document.body.contains(elem)) {
-		try {
-			_cb();
-		} catch(e) {
-			console.log(e);
-		}
-	}
+function isElementExist(_el, _cb) {
+    var elem = document.querySelector(_el)
+
+    if (document.body.contains(elem)) {
+        try {
+            _cb()
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }
 
 function isWebp() {
     function testWebP(callback) {
-
         var webP = new Image()
         webP.onload = webP.onerror = function () {
             callback(webP.height == 2)
-        };
-        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"
+        }
+        webP.src =
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
     }
-    
+
     testWebP(function (support) {
-    
         if (support == true) {
             document.querySelector('body').classList.add('webp')
         } else {
@@ -35,3 +36,20 @@ function isWebp() {
     })
 }
 
+function initMenu() {
+    const body = document.querySelector('body')
+    const button = document.querySelector('#nav-opener')
+    const content = document.querySelector('#header-content')
+
+    button.addEventListener('click', () => {
+        body.classList.toggle('nav-active')
+        content.classList.toggle('active')
+        button.classList.toggle('active')
+    })
+
+    window.addEventListener('resize', () => {
+        body.classList.remove('nav-active')
+        content.classList.remove('active')
+        button.classList.remove('active')
+    })
+}
